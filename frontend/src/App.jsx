c95bec8f5a0c17a2359dd2dd8dfc4fb3d40d253b
bhanu8988 <bhanuteja8988@gmail.com>
2025-02-13 22:21:01 +0530

    commited and added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import {LoginPage} from './Routes.jsx'
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+    </BrowserRouter>
+ 
+  );
+}
+
+
+export default App;
